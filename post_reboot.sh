d3a2a55ef2f2ce786698a666492ac0c184b0d39c
jadesro <jacques@desroches.ca>
2025-08-10 20:14:54 -0400

    Create post_reboot.sh

---
+++ post_reboot.sh
@@ -0,0 +1,24 @@
+# Install Omarchy
+sudo pacman -S wget
+wget -qO- https://omarchy.org/install | bash
+
+# remove apps that Omarchy installed that we don't want
+sudo pacman -Rcns 1password-beta 1password-cli
+
+yay -S --noconfirm --needed \
+	pkgfile \
+  pika-backup \
+  bind \
+	uv \
+	bind \
+	os-prober \
+	gvfs-smb \
+	ntfs-3g \
+  timeshift timeshift-autosnap \
+	bitwarden bitwarden-cli
+
+ # Install new grub theme
+ git clone https://github.com/vinceliuice/grub2-themes.git
+ cd grub2-themes
+ # this theme installer will update grub and will also intall the grub snapshot view as per above
+ sudo ./install.sh -b -t tela
