3514d763f0b6fee58ed96a12b23209e622dfcb41
jadesro <jacques@desroches.ca>
2025-08-13 13:31:45 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -206,7 +206,9 @@ fi
 # the GRUB_CMDLINE_LINUX_DEFAULT should now have the argument
 #"loglevel=3 quiet cryptdevice=UUID=xxxxxxxxxxxx:main root=/dev/mapper/main"
 # note the ":main" text after the UUID
-sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:main root=/dev/mapper/main %g" /etc/default/grub
+
+export ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value "${CRYPT}")
+sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:main root=/dev/mapper/main%g" /etc/default/grub
 #  mkinitcpio -p linux
 # if we get an error (can't write to /boot), we need to remount boot as read/write and rerun the command
 #mount -n -o remount,rw /boot
