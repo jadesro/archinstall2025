347ba7784a9e42b60d48285c6c6c2d58896b3985
jadesro <jacques@desroches.ca>
2025-08-13 19:26:39 -0400

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -2,19 +2,23 @@
 sudo pacman -Syu zram-generator
 
 # create file at /etc/systemd/zram-generator.conf
+cat > /etc/systemd/zram-generator.conf << EOF
 [zram0]
 zram-size = ram / 2
 compression-algorithm = zstd
 swap-priority = 100
 fs-type = swap
+EOF
 
 # Install Timeshift
 yay -S timeshift timeshift-autosnap
 sudo timeshift --list-devices
+
 # Create a first snapshot
-sudo timeshift --create --comments "[xx] Initial Snapshot" --tags D
+sudo timeshift --create --comments "[$(date +%Y-%m-%d)] Initial Snapshot" --tags D
 # change ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots for ExecStart=/usr/bin/grub-btrfsd --syslog -t
-sudo systemctl edit --full grub-btrfsd
+# sudo systemctl edit --full grub-btrfsd
+sed -i 's/ExecStart=\/usr\/bin\/grub-btrfsd --syslog \/.snapshots/ExecStart=\/usr\/bin\/grub-btrfsd --syslog -t/' /etc/systemd/system/grub-btrfsd.service
 
 # regenerate the grub config so that the snapshot are visible on boot
 sudo grub-mkconfig -o /boot/grub/grub.cfg
@@ -43,4 +47,5 @@ yay -S --noconfirm --needed \
  git clone https://github.com/vinceliuice/grub2-themes.git
  cd grub2-themes
  # this theme installer will update grub and will also intall the grub snapshot view as per above
- sudo ./install.sh -b -t tela
+ s1Kermit1
+ udo ./install.sh -b -t tela
