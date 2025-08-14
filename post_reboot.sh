1ba6c3fd879e75d8e91cfd438526663e2b7ce4a3
jadesro <jacques@desroches.ca>
2025-08-14 11:49:56 -0400

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -1,4 +1,7 @@
 # Install zram
+echo -ne "################################################"
+echo -ne "############## Install ZRAM  ###################"
+echo -ne "################################################"
 sudo pacman -Syu zram-generator
 
 # create file at /etc/systemd/zram-generator.conf
@@ -10,26 +13,43 @@ swap-priority = 100
 fs-type = swap
 EOF
 
+# Install yay
+echo -ne "################################################"
+echo -ne "############### Install yay ####################"
+echo -ne "################################################"
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+sudo pacman -S base-devel
+makepkg -si
+
 # Install Timeshift
-yay -S timeshift timeshift-autosnap
+echo -ne "################################################"
+echo -ne "############ Install timeshift #################"
+echo -ne "################################################"
+yay -S --noconfirm --needed timeshift timeshift-autosnap
 sudo timeshift --list-devices
 
 # Create a first snapshot
+echo -ne "Create first snapshot"
 sudo timeshift --create --comments "[$(date +%Y-%m-%d)] Initial Snapshot" --tags D
 # change ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots for ExecStart=/usr/bin/grub-btrfsd --syslog -t
 # sudo systemctl edit --full grub-btrfsd
 sed -i 's/ExecStart=\/usr\/bin\/grub-btrfsd --syslog \/.snapshots/ExecStart=\/usr\/bin\/grub-btrfsd --syslog -t/' /etc/systemd/system/grub-btrfsd.service
 
 # regenerate the grub config so that the snapshot are visible on boot
+echo "Make snapshots visible on next boot"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install Omarchy
+# Download Omarchy
 sudo pacman -S wget
-wget -qO- https://omarchy.org/install | bash
+wget -qO- https://omarchy.org/install
 
 # remove apps that Omarchy installed that we don't want
 sudo pacman -Rcns 1password-beta 1password-cli
 
+echo -ne "################################################"
+echo -ne "########### Install prefered apps ##############"
+echo -ne "################################################"
 yay -S --noconfirm --needed \
 	pkgfile \
 	pika-backup \
